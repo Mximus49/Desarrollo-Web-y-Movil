d012c5ee5b9a599d44ce11cc411d9bea0b56e3fa
Máximo Feliú <164103871+Mximus49@users.noreply.github.com>
2026-09-04 18:22:56 -0400

    Add files via upload

---
+++ semana5/models/usuario.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const usuarioSchema = mongoose.Schema({
+    run: String,
+    nombreCompleto: String,
+    direccion: String,
+    comuna: String,
+    provincia: String,
+    region: String,
+    fechaNacimiento: String,
+    sexo: String,
+    correo: String,
+    telefono: String,
+    pass: String,
+    perfil: {type: String, enum: ['admin', 'usuario','cajero'], default: 'usuario'},
+    correoValidado: {type: Boolean, default: false}
+});
+
+module.exports = mongoose.model('Usuario', usuarioSchema);
